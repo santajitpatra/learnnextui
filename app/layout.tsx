74d3b72b03fe8699b795eeb2e57f31208da888a3
Santajit Patra <business.santajitpatra@gmail.com>
2023-10-21 02:26:06 +0530

    fix : unavoidably
    content is unavoidably different between the server and the client
    and added color palate blue-Cyan

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,9 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className='dark'>
+    <html lang="en" className='dark' style={{ colorScheme: "dark" }} 
+    //suppressHydrationWarning={true}
+    >
       <body className={inter.className}>
         <Providers>
           <header className='py-6'>
